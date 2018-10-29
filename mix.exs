363b53c2342b29230cca7e7a19f5e12534bec4f7
Manolis Kousanakis <ekousanakis@gmail.com>
2018-10-29 17:27:42 +0200

    feat(app): update to support session configuration of exrabbitmq

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExRabbitMQ.RPC.Mixfile do
   def project do
     [
       app: :ex_rabbitmq_rpc,
-      version: "1.1.0",
+      version: "2.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -21,11 +21,12 @@ defmodule ExRabbitMQ.RPC.Mixfile do
   defp deps do
     [
       {:uuid, "~> 1.1"},
-      {:exrabbitmq, "~> 2.11"},
-      {:credo, "~> 0.8", runtime: false},
-      {:dialyxir, "~> 0.5", runtime: false},
+      # {:exrabbitmq, "~> 2.11"},
+      {:exrabbitmq, github: "StoiximanServices/exrabbitmq", branch: "multiple_declarations"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.19", runtime: false, override: true},
-      {:poison, "~> 3.1", only: :test}
+      {:poison, "~> 3.1", runtime: false}
     ]
   end
 end
--- test/ex_rabbit_rpc_test.exs
+++ test/ex_rabbit_rpc_test.exs
@@ -1,5 +1,4 @@
 defmodule Payload do
-
   defstruct [
     :data,
     delay: 0
@@ -10,7 +9,7 @@ defmodule ExRabbitMQ.RPCTest do
   use ExUnit.Case, async: false
 
   setup_all do
-    {:ok, _pid} = ExRabbitMQ.Connection.Supervisor.start_link()
+    {:ok, _pid} = ExRabbitMQ.Connection.Pool.Supervisor.start_link([])
     :ok
   end
 
@@ -51,22 +50,20 @@ defmodule ExRabbitMQ.RPCTest do
     GenServer.stop(client)
     GenServer.stop(server)
   end
-
 end
 
 defmodule TestClient do
-
   use GenServer
   use ExRabbitMQ.RPC.Client
 
   @timeout 2000
 
   def start() do
-    GenServer.start(__MODULE__, [test_pid: self()])
+    GenServer.start(__MODULE__, test_pid: self())
   end
 
-  def init([test_pid: test_pid]) do
-    setup_client(:test, %{test_pid: test_pid})
+  def init(test_pid: test_pid) do
+    setup_client(:test_connection, %{test_pid: test_pid})
   end
 
   def test_request(client, payload, correlation_id) do
@@ -74,7 +71,7 @@ defmodule TestClient do
   end
 
   def handle_cast({:test_request, payload, correlation_id}, %{test_pid: test_pid} = state) do
-    queue = Application.get_env(:exrabbitmq, :test)[:queue]
+    queue = Application.get_env(:exrabbitmq, :test_session)[:queue]
     payload = Poison.encode!(payload)
     result = request(payload, "", queue, correlation_id: correlation_id, expiration: @timeout)
     send(test_pid, {:send_request, result})
@@ -91,28 +88,29 @@ defmodule TestClient do
     send(test_pid, {:received_response, correlation_id, result})
     {:noreply, state}
   end
-
 end
 
 defmodule TestServer do
-
   use GenServer
   use ExRabbitMQ.RPC.Server
 
   def start() do
-    GenServer.start(__MODULE__, [test_pid: self()])
+    GenServer.start(__MODULE__, test_pid: self())
   end
 
-  def init([test_pid: test_pid]) do
-    {:ok, state} = setup_server(:test, :test, %{test_pid: test_pid})
-    {:ok, state}
+  def init(test_pid: test_pid) do
+    setup_server(:test_connection, :test_session, %{test_pid: test_pid})
   end
 
-  def handle_request(bin_payload, %{correlation_id: correlation_id} = metadata, %{test_pid: test_pid} = state) do
+  def handle_request(bin_payload, metadata, state) do
+    %{correlation_id: correlation_id} = metadata
+    %{test_pid: test_pid} = state
     payload = %Payload{delay: delay} = Poison.decode!(bin_payload, as: %Payload{})
     send(test_pid, {:received_request, correlation_id, payload})
+
     if delay == 0 do
-      {:respond, bin_payload, state} # Echo back the payload.
+      # Echo back the payload.
+      {:respond, bin_payload, state}
     else
       Process.send_after(self(), {:respond, metadata, bin_payload}, delay)
       {:noreply, state}
@@ -128,5 +126,4 @@ defmodule TestServer do
     xrmq_basic_ack(tag, state)
     {:noreply, state}
   end
-
 end
