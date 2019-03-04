a193f6bd6d426a0e6523f21411df3d3e08c4cc8f
Dennis Triantafyllis <dennistriantafyllis@gmail.com>
2019-03-04 11:03:29 +0200

    chore(app): updated deps
    - updated version
    - fixed credo and dialyzer warnings

--- lib/ex_rabbitmq/rpc/client/options.ex
+++ lib/ex_rabbitmq/rpc/client/options.ex
@@ -1,9 +1,9 @@
 defmodule ExRabbitMQ.RPC.Client.Options do
   @moduledoc false
 
-  alias ExRabbitMQ.Config.{Queue, Session}
-
   import UUID, only: [uuid4: 0]
+
+  alias ExRabbitMQ.Config.{Queue, Session}
 
   @default_queue_prefix "rpc.gen-"
 
@@ -16,13 +16,52 @@ defmodule ExRabbitMQ.RPC.Client.Options do
   """
   def get_session_config(opts) do
     case Keyword.get(opts, :session, nil) do
-      nil ->
-        get_default_session_config(opts)
-
-      session ->
-        session
+      nil -> get_default_session_config(opts)
+      session -> session
     end
   end
+
+  @doc """
+  Returns the `AMQP.Basic.publish` options.
+
+  Sets the options for `correlation_id`, `reply_to` and `expiration` as specified in the arguments.
+  Also set the `timestamp` to the current time.
+  """
+  def get_publish_options(opts, correlation_id, reply_to, expiration) do
+    expiration = if expiration > 0, do: to_string(expiration), else: :undefined
+
+    opts
+    |> Keyword.put(:correlation_id, correlation_id)
+    |> Keyword.put(:reply_to, reply_to)
+    |> Keyword.put(:expiration, expiration)
+    |> Keyword.put_new_lazy(:timestamp, &get_unix_now/0)
+  end
+
+  @doc """
+  Returns the correlation_id for the request.
+
+  Defaults to a random generated one.
+  """
+  def get_correlation_id(opts), do: do_get_correlation_id(opts[:correlation_id])
+
+  @doc """
+  Returns the expiration time in milliseconds for the request.
+
+  Defaults to `5000` milliseconds.
+  """
+  def get_expiration(opts), do: do_get_expiration(opts[:expiration])
+
+  @doc """
+  Returns the `from` process that the response should be replied to.
+
+  Defaults to `nil`.
+  """
+  def get_call_from(opts), do: do_get_call_from(opts[:call_from])
+
+  @doc """
+  Sets in the options the `from` process that the response should be replied to.
+  """
+  def set_call_from(opts, from), do: Keyword.put(opts, :call_from, from)
 
   # Returns the default session configuration that declares the `reply-to` queue
   # plus any extra declarations from the :declarations option.
@@ -57,57 +96,15 @@ defmodule ExRabbitMQ.RPC.Client.Options do
     end
   end
 
-  @doc """
-  Returns the `AMQP.Basic.publish` options.
-
-  Sets the options for `correlation_id`, `reply_to` and `expiration` as specified in the arguments.
-  Also set the `timestamp` to the current time.
-  """
-  def get_publish_options(opts, correlation_id, reply_to, expiration) do
-    expiration = if expiration > 0, do: to_string(expiration), else: :undefined
-
-    opts
-    |> Keyword.put(:correlation_id, correlation_id)
-    |> Keyword.put(:reply_to, reply_to)
-    |> Keyword.put(:expiration, expiration)
-    |> Keyword.put_new_lazy(:timestamp, &get_unix_now/0)
-  end
-
   defp get_unix_now, do: DateTime.utc_now() |> DateTime.to_unix(:millisecond)
-
-  @doc """
-  Returns the correlation_id for the request.
-
-  Defaults to a random generated one.
-  """
-  def get_correlation_id(opts), do: do_get_correlation_id(opts[:correlation_id])
 
   defp do_get_correlation_id(nil), do: @default_correlation_prefix <> uuid4()
   defp do_get_correlation_id(value), do: value
-
-  @doc """
-  Returns the expiration time in milliseconds for the request.
-
-  Defaults to `5000` milliseconds.
-  """
-  def get_expiration(opts), do: do_get_expiration(opts[:expiration])
 
   defp do_get_expiration(value) when is_number(value) and value < 1, do: 0
   defp do_get_expiration(value) when is_number(value), do: value
   defp do_get_expiration(_), do: @default_expiration
 
-  @doc """
-  Returns the `from` process that the response should be replied to.
-
-  Defaults to `nil`.
-  """
-  def get_call_from(opts), do: do_get_call_from(opts[:call_from])
-
   defp do_get_call_from({pid, _tag} = from) when is_pid(pid), do: from
   defp do_get_call_from(_), do: nil
-
-  @doc """
-  Sets in the options the `from` process that the response should be replied to.
-  """
-  def set_call_from(opts, from), do: Keyword.put(opts, :call_from, from)
 end
