093ad1a82478fb0032663a527cb94aba57e18576
Dennis Triantafyllis <dennistriantafyllis@gmail.com>
2019-03-04 11:03:29 +0200

    chore(app): updated deps
    - updated version
    - fixed credo and dialyzer warnings

--- config/test.exs
+++ config/test.exs
@@ -5,11 +5,15 @@ config :logger, level: :error
 config :exrabbitmq, :test_connection,
   username: "guest",
   password: "guest",
-  host: "localhost",
+  host: "localhost"
 
 config :exrabbitmq, :test_session,
   queue: "ex_rabbitmq_rpc_test",
   consume_opts: [no_ack: false],
   declarations: [
-    {:queue, [name: "ex_rabbitmq_rpc_test", queue_opts: [auto_delete: true]]}
+    {:queue,
+     [
+       name: "ex_rabbitmq_rpc_test",
+       queue_opts: [auto_delete: true]
+     ]}
   ]
